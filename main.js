087559eb2d1c903ed833c0e7bd9fd2e179d1bdf2
Jefferson <jeffabuy@gmail.com>
2024-04-08 18:10:44 +0300

    finished project

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 import "./style.css";
-import {
-  getWeather,
-  getTime,
-  getLatLong,
-  getCondition,
-  conditionIcon,
-} from "./utils.js";
+import { getWeather, getTime, getCondition, setWeather } from "./utils.js";
+import moment from "moment";
 
 const form = document.getElementById("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const cityInput = document.getElementById("cityInput").value;
-  createHTML(cityInput);
+  createHTML();
 });
 
 async function createHTML() {
@@ -22,72 +16,108 @@ async function createHTML() {
     getCondition(),
   ]);
 
+  console.log(time, weather, condition);
+  const cityInput = document.getElementById("cityInput").value;
   const app = await document.querySelector("#app");
+  console.log(moment.unix(weather.sunrise).format("LT"));
+  console.log(moment.unix(weather.sunset).format("LT"));
 
-  app.innerHTML = `<div class="bg-slate-500 bg-opacity-15 rounded-xl w-screen m-10">
-  <h1 class="text-6xl font-extrabold p-5">Weather App</h1>
-  <form id="form">
-    <label class="flex justify-center items-center mb-5"
-      ><span class="material-symbols-outlined text-4xl">
-        location_on
-      </span>
-      <input
-        type="text"
-        id="cityInput"
-        value="${cityInput.value}"
-        placeholder="${cityInput.value}"
-        class="bg-transparent w-48 font-bold py-2 text-center rounded-lg text-2xl outline-none placeholder:flex placeholder:font-bold placeholder:text-center placeholder:text-black placeholder:pl-5 placeholder:text-2xl"
-      />
-    </label>
-  </form>
-  <div class="flex flex-col justify-center items-center">
-    <h1 class="mb-5">${time.day_of_week} ${time.day}, ${time.hour}:${time.minute}</h1>
-    <img
-      id="weatherIcon"
-      class="max-h-80"
-      src=""
-    />
-    <h1 class="text-4xl font-bold m-5">${weather.temp}&deg;</h1>
-    <h1 id="conditionName" class="font-semibold text-1xl"> </h1>
-    <div class="flex justify-center gap-5 m-10">
-      <h1 class="flex justify-center">
-        <span class="material-symbols-outlined text-red-500">
-          arrow_downward_alt </span
-        >L: ${weather.min_temp}&deg;
-      </h1>
-      <h1>Feels Like: ${weather.feels_like}&deg;</h1>
-      <h1 class="flex justify-center">
-        <span class="material-symbols-outlined text-green-500">
-          arrow_upward_alt </span
-        >H: ${weather.max_temp}&deg;
-      </h1>
-    </div>
+  app.innerHTML = `<header class="p-10">
+  <label class="relative">
+  <form id="form" class="flex justify-between">
+  <input
+    type="text"
+    class="bg-transparent pl-10 py-3 text-xl placeholder:"
+    id="cityInput"
+    value=""
+    placeholder="${cityInput}"
+  />
+  <img
+    src="/public/Icons/Group.png"
+    class="absolute top-4 left-4 z-10 h-5"
+  />
+  <form />
+</label>
+<div class="flex items-center">
+<h1 class="text-4xl font-thin italic">Weather App</h1>
+</div>
+</header>
+<main class="flex flex-col items-center">
+<h1 class="font-bold text-4xl tracking-widest pt-5">
+  ${cityInput}
+</h1>
+<h1>${time.day_of_week} ${time.day}, ${time.hour}:${time.minute}</h1>
+<h1 id="conditionName" class="font-thin text-4xl tracking-widest pt-8"></h1>
+<img id="weatherIcon" class="size-36 m-10" src="" />
+<h1 class="font-thin text-7xl">${weather.temp}&deg;</h1>
+<div class="flex gap-10 my-10">
+  <div class="flex flex-col justify-center items-center gap-5">
+    <h1 class="flex justify-center">
+      <span class="material-symbols-outlined text-red-500">
+        arrow_downward_alt </span
+      >min
+    </h1>
+    <h2>${weather.min_temp}&deg;</h2>
   </div>
-  <div class="flex gap-10 justify-center">
-    <div class="flex flex-col items-center">
-      <span class="material-symbols-outlined"> water_drop </span>
-      <h2>${weather.humidity}%</h2>
-      <h2>Humidity</h2>
-    </div>
-    <div class="flex flex-col items-center">
-      <span class="material-symbols-outlined"> rainy </span>
-      <h2>${weather.cloud_pct}%</h2>
-      <h2>Precipitation</h2>
-    </div>
-    <div class="flex flex-col items-center">
-      <span class="material-symbols-outlined"> air </span>
-      <h2>${weather.wind_speed} km/h</h2>
-      <h2>Wind Speed</h2>
-    </div>
+  <div
+    class="flex flex-col border-gray-400 justify-center border"
+  ></div>
+  <div class="flex flex-col justify-center items-center gap-5">
+    <h1 class="flex justify-center">
+      <span class="material-symbols-outlined text-green-500">
+        arrow_upward_alt </span
+      >max
+    </h1>
+    <h2>${weather.max_temp}&deg;</h2>
   </div>
-</div>`;
-
-  const icon = await conditionIcon();
+</div>
+<hr />
+<div class="flex w-full justify-evenly items-center p-12">
+  <div class="flex flex-col items-center pt-2">
+    <span class="material-symbols-outlined pb-2"> water_drop </span>
+    <h2>${weather.humidity}%</h2>
+    <h2>Humidity</h2>
+  </div>
+  <div
+    class="flex flex-col border-gray-400 h-14 justify-center border"
+  ></div>
+  <div class="flex flex-col items-center">
+    <img class="h-10" src="/IconPack/Frame 10.png" />
+    <h2>${weather.cloud_pct}%</h2>
+    <h2>Precipitation</h2>
+  </div>
+  <div
+    class="flex flex-col border-gray-400 h-14 justify-center border"
+  ></div>
+  <div class="flex flex-col items-center">
+    <img class="h-10" src="/IconPack/Frame 11.png" />
+    <h2>${weather.wind_speed} km/h</h2>
+    <h2>Wind Speed</h2>
+  </div>
+  <div
+    class="flex flex-col border-gray-400 h-14 justify-center border"
+  ></div>
+  <div class="flex flex-col items-center">
+    <img class="h-10" src="/IconPack/Frame 13.png" />
+    <h2>${moment.unix(weather.sunrise).format("LT")}</h2>
+    <h2>Sunrise</h2>
+  </div>
+  <div
+    class="flex flex-col border-gray-400 h-14 justify-center border"
+  ></div>
+  <div class="flex flex-col items-center">
+    <img class="h-10" src="/IconPack/Frame 12.png" />
+    <h2>${moment.unix(weather.sunset).format("LT")}</h2>
+    <h2>Sunset</h2>
+  </div>
+</div>
+</main>`;
   const form = document.getElementById("form");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const cityInput = document.getElementById("cityInput").value;
-    createHTML(cityInput);
+    createHTML();
   });
+
+  setWeather(condition);
 }
--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ export async function getWeather() {
     {
       method: "GET",
       headers: {
-        "X-Api-Key": "bbHEJw2wL/Mj/8oJLWZjZg==PPGuSGAVow0sfsLi",
+        "X-Api-Key": "tL8D8cQ4nv4rQXcR++vOEA==6PjurvtGvLucGDdx",
         "Content-Type": "application/json",
       },
     },
@@ -15,13 +15,13 @@ export async function getWeather() {
 }
 
 export async function getTime() {
-  const cityInput = document.getElementById("cityInput").value;
+  const cityInput = document.getElementById("cityInput");
   const response = await fetch(
-    `https://api.api-ninjas.com/v1/worldtime?city=${cityInput}`,
+    `https://api.api-ninjas.com/v1/worldtime?city=${cityInput.value}`,
     {
       method: "GET",
       headers: {
-        "X-Api-Key": "bbHEJw2wL/Mj/8oJLWZjZg==PPGuSGAVow0sfsLi",
+        "X-Api-Key": "tL8D8cQ4nv4rQXcR++vOEA==6PjurvtGvLucGDdx",
         "Content-Type": "application/json",
       },
     },
@@ -30,70 +30,62 @@ export async function getTime() {
   return time;
 }
 
-export async function getLatLong() {
+export async function getCondition() {
   const cityInput = document.getElementById("cityInput").value;
   const response = await fetch(
     `https://api.api-ninjas.com/v1/city?name=${cityInput}`,
     {
       method: "GET",
       headers: {
-        "X-Api-Key": "bbHEJw2wL/Mj/8oJLWZjZg==PPGuSGAVow0sfsLi",
+        "X-Api-Key": "tL8D8cQ4nv4rQXcR++vOEA==6PjurvtGvLucGDdx",
         "Content-Type": "application/json",
       },
     },
   );
   const latlong = await response.json();
-  return latlong;
-}
 
-export async function getCondition() {
-  const latlong = await getLatLong();
-
-  const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${latlong[0].latitude}&longitude=${latlong[0].longitude}&current=weather_code`,
+  const response2 = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latlong[0].latitude}&longitude=${latlong[0].longitude}&current=weather_code&hourly=weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min`,
   );
-  const condition = await response.json();
+  const condition = await response2.json();
   return condition;
 }
 
-export async function conditionIcon() {
-  const condition = await getCondition();
-
+export function setWeather(condition) {
   const imageElement = document.getElementById("weatherIcon");
   const conditionName = document.getElementById("conditionName");
-
   if (condition.current.weather_code === 0) {
-    imageElement.src = "/Icons/day_clear.png";
+    imageElement.src = "/WeatherPack/Sunny.png";
     conditionName.textContent = "Clear Sky";
   } else if (condition.current.weather_code === 1) {
-    imageElement.src = "/Icons/day_clear.png";
+    imageElement.src = "/WeatherPack/Sunny.png";
     conditionName.textContent = "Mainly Clear";
   } else if (condition.current.weather_code === 2) {
-    imageElement.src = "/Icons/day_partial_cloud.png";
+    imageElement.src = "/WeatherPack/Partly-cloudy.png.png.png";
     conditionName.textContent = "Party Cloudy";
   } else if (condition.current.weather_code === 3) {
-    imageElement.src = "/Icons/overcast.png";
+    imageElement.src = "/WeatherPack/Cloudy.png";
     conditionName.textContent = "Overcast";
-  } else if ((condition.current.weather_code === 45, 48)) {
-    imageElement.src = "/Icons/fog.png";
+  } else if (condition.current.weather_code === 45) {
+    imageElement.src = "/WeatherPack/Fog.png";
     conditionName.textContent = "Foggy";
-  } else if ((condition.current.weather_code === 51, 53, 55)) {
-    imageElement.src = "/Icons/day_rain.png";
+  } else if (condition.current.weather_code === 51) {
+    imageElement.src = "/WeatherPack/Drizzle.png";
     conditionName.textContent = "Drizzle";
-  } else if ((condition.current.weather_code === 61, 63, 65)) {
-    imageElement.src = "/Icons/rain.png";
+  } else if (condition.current.weather_code === 61) {
+    imageElement.src = "/WeatherPack/Rain.png";
     conditionName.textContent = "Rain";
-  } else if ((condition.current.weather_code === 71, 73, 75)) {
-    imageElement.src = "/Icons/snow.png";
+  } else if (condition.current.weather_code === 71 || 73 || 75) {
+    imageElement.src = "/WeatherPack/Snow.png";
     onditionName.textContent = "Snow";
   } else if ((condition.current.weather_code === 80, 81, 82)) {
-    imageElement.src = "/Icons/rain.png";
+    imageElement.src = "/WeatherPack/Heavy-rain.png";
     conditionName.textContent = "Rain showers";
   } else if (condition.current.weather_code === 95) {
-    imageElement.src = "/Icons/thunder.png";
+    imageElement.src = "/WeatherPack/Sever-thunderstorm.png";
     conditionName.textContent = "Light Thunderstorm";
   } else if ((condition.current.weather_code === 96, 99)) {
-    imageElement.src = "/Icons/rain_thunder.png";
+    imageElement.src = "/WeatherPack/Rain&Thunderstorm.png";
     conditionName.textContent = "Heavy Thunderstorm";
   }
 }
